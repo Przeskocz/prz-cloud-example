1ba6c7a4c0448a2dc34496b905e9a3e20fef3730
przeskocz <skoczp@gmail.com>
2020-04-24 09:26:36 +0200

    checkpoint

--- src/test/java/prz/cloud/example/cutting/stock/ApplicationTests.java
+++ src/test/java/prz/cloud/example/cutting/stock/ApplicationTests.java
@@ -1,12 +1,12 @@
 package prz.cloud.example.cutting.stock;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
